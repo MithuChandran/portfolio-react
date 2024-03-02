3236b89f18879fa5447e5f84e3187a4e55edc177
mithuna <mithunachandrasekaran@gmail.com>
2024-03-02 17:20:37 +0530

    Refactor Modal component layout

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ activeID, setShowModal }) => {
   const portfolio = portfolios.find((portfolio) => portfolio.id === activeID);
   return (
     <div className="w-full h-full fixed top-0 left-0 z-10 bg-headingColor bg-opacity-40">
-      <div className="max-w-[600px] absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 p-5">
+      <div className="w-11/12 md:max-w-[600px] md:w-full absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 p-5">
         <div>
           <figure>
             <img className="rounded-[8px]" src={portfolio.imgUrl} alt="" />
@@ -37,17 +37,14 @@ const Modal = ({ activeID, setShowModal }) => {
             </button>
           </a>
         </div>
-        <a href={portfolio.siteUrl}>
-          <button className="bg-primaryColor text-white py-2 px-4 my-8 rounded-[8px] font-[500] hover:bg-headingColor ease-in duration-300"></button>
-        </a>
-      </div>
 
-      <button
-        onClick={() => setShowModal(false)}
-        className="w-[1.8rem] h-[1.8rem] bg-[white] absolute top-[1.7rem] right-[1.7rem] text-[25px] flex items-center justify-center rounded-[3px] leading-0 cursor-pointer"
-      >
-        &times;
-      </button>
+        <button
+          onClick={() => setShowModal(false)}
+          className="w-[1.8rem] h-[1.8rem] bg-[white] absolute top-[1.7rem] right-[1.7rem] text-[25px] flex items-center justify-center rounded-[3px] leading-0 cursor-pointer"
+        >
+          &times;
+        </button>
+      </div>
     </div>
   );
 };
