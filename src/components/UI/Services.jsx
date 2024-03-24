eff3db5dbc4e65be493938c16fa632fe672cbbad
mithuna <mithunachandrasekaran@gmail.com>
2024-03-25 01:05:32 +0530

    Update Services component with new skills and technologies

--- src/components/UI/Services.jsx
+++ src/components/UI/Services.jsx
@@ -10,14 +10,15 @@ const Services = () => {
       <div className="container lg:pt-5">
         <div className="text-center">
           <h2 className="text-headingColor font-[800] text-[2.4rem] mb-5">
-            What do I help
+            What can I do?
           </h2>
           <p className="lg:max-w-[600px] lg:mx-auto text-headingColor font-[500] text-[16px] leading-7">
-            Experience with HTML, CSS, and JavaScript. Also, I have knowledge of
-            Figma and Adobe XD. Proven ability to learn new technologies quickly
-            and apply them to real-world projects. Seeking an opportunity to
-            contribute to a team building innovative and
-            user-friendly applications.
+            Passionate about front-end development, web design, and machine
+            learning. Experience with Java, Python, HTML, and CSS. Also, I have
+            knowledge of using Jupyter Notebook. Proven ability to learn new
+            technologies quickly and apply them to real-world projects. Seeking
+            an opportunity to contribute to a team building innovative and
+            user-friendly applications.
           </p>
         </div>
 
@@ -41,10 +42,9 @@ const Services = () => {
                           Frontend Development
                         </h3>
                         <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
-                          Experience with HTML, CSS, and JavaScript. Also, I
-                          have knowledge of Figma and Adobe XD. Proven ability
-                          to learn new technologies quickly and apply them to
-                          real-world projects.
+                          Experience with Dart language. Also, I have knowledge
+                          of React. Proven ability to learn new technologies
+                          quickly and apply them to real-world projects.
                         </p>
                       </div>
                     </div>
@@ -66,17 +66,16 @@ const Services = () => {
                       <div
                         data-aos="fade-left"
                         data-aos-delay="50"
-                        data-aos-duration="1300"
+                        data-aos-duration="1500"
                         className="bg-white p-4 rounded shadow-lg shadow-primaryColor group hover:bg-primaryColor cursor-pointer ease-in duration-150"
                       >
                         <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl">
-                          Data Analysis
+                          Web Development
                         </h3>
                         <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
-                          Experience with HTML, CSS, and JavaScript. Also, I
-                          have knowledge of Figma and Adobe XD. Proven ability
-                          to learn new technologies quickly and apply them to
-                          real-world projects.
+                          Experience with HTML, and CSS. Also, I have knowledge
+                          of JavaScript. Proven ability to learn new
+                          technologies and apply them to real-world projects.
                         </p>
                       </div>
                     </div>
@@ -84,12 +83,11 @@ const Services = () => {
 
                   <div className="rounded-full bg-primaryColor border-white borde-4 w-10 h-10 absolute left-1/2 transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
                     <figure>
-                      <img src={backendImg} alt="" />
+                      <img src={appsImg} alt="" />
                     </figure>
                   </div>
                 </div>
               </div>
-
               {/* =============== left card===============*/}
               <div className="mt-6 sm:mt-0 sm:mb-12">
                 <div className="flex items-center flex-col sm:flex-row">
@@ -101,13 +99,13 @@ const Services = () => {
                         className="bg-white p-4 rounded shadow-lg shadow-primaryColor group hover:bg-primaryColor cursor-pointer ease-in duration-150"
                       >
                         <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl">
-                          UI/UX Design
+                          Machine Learning
                         </h3>
                         <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
-                          Experience with HTML, CSS, and JavaScript. Also, I
-                          have knowledge of Figma and Adobe XD. Proven ability
-                          to learn new technologies quickly and apply them to
-                          real-world projects.
+                          Experience with python. Also, I have knowledge of R
+                          and Jupytor Notebook. Proven ability to learn new
+                          technologies quickly and apply them to real-world
+                          projects.
                         </p>
                       </div>
                     </div>
@@ -116,38 +114,6 @@ const Services = () => {
                   <div className="rounded-full bg-primaryColor border-white borde-4 w-10 h-10 absolute left-1/2 transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
                     <figure>
                       <img src={uiImg} alt="" />
-                    </figure>
-                  </div>
-                </div>
-              </div>
-
-              {/* =============== right card===============*/}
-              <div className="mt-6 sm:mt-0 sm:mb-12">
-                <div className="flex items-center flex-col sm:flex-row">
-                  <div className="flex justify-end w-full mx-auto items-center">
-                    <div className="w-full sm:w-1/2 sm:pl-8">
-                      <div
-                        data-aos="fade-left"
-                        data-aos-delay="50"
-                        data-aos-duration="1500"
-                        className="bg-white p-4 rounded shadow-lg shadow-primaryColor group hover:bg-primaryColor cursor-pointer ease-in duration-150"
-                      >
-                        <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl">
-                          Web Development
-                        </h3>
-                        <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
-                          Experience with HTML, CSS, and JavaScript. Also, I
-                          have knowledge of Figma and Adobe XD. Proven ability
-                          to learn new technologies quickly and apply them to
-                          real-world projects.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="rounded-full bg-primaryColor border-white borde-4 w-10 h-10 absolute left-1/2 transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
-                    <figure>
-                      <img src={appsImg} alt="" />
                     </figure>
                   </div>
                 </div>
